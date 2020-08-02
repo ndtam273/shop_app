9a00d37a92f9e437e1407c409a26386d09eb3ad5
tam nguyen <ndtam.273@gmail.com>
2020-08-02 21:55:30 +0700

    wrwrr

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Anton',
-          textTheme: ThemeData.light().textTheme.copyWith(
-                bodyText1: TextStyle(
-                  color: Color.fromRGBO(20, 51, 51, 1),
-                ),
-                bodyText2: TextStyle(
-                  color: Color.fromRGBO(30, 20, 49, 1),
-                ),
-                headline6: TextStyle(
-                    fontSize: 20,
-                    fontFamily: 'Lato',
-                    fontWeight: FontWeight.bold),
-              )),
-      home: ProductsOverviewScreen(),
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            canvasColor: Color.fromRGBO(255, 254, 229, 1),
+            fontFamily: 'Anton',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                  bodyText1: TextStyle(
+                    color: Color.fromRGBO(20, 51, 51, 1),
+                  ),
+                  bodyText2: TextStyle(
+                    color: Color.fromRGBO(30, 20, 49, 1),
+                  ),
+                  headline6: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Lato',
+                      fontWeight: FontWeight.bold),
+                )),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
+      ),
     );
   }
 }
